2567b7ad38f0718cdc867138fc0adcba3d3e4a6b
Martijn van Duijneveldt <martijnvanduijneveldt@gmail.com>
2019-05-23 17:01:03 +0200

    Updated pokemon selection button

--- src/app/modules/userio/user-i-o.module.ts
+++ src/app/modules/userio/user-i-o.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {UserIOComponent} from './user-i-o.component';
 import {LogDisplayerComponent} from '../../components/log-displayer/log-displayer.component';
-import {MoveSelectorComponent} from "../../components/move-selector/move-selector.component";
+import {MoveSelectorComponent} from '../../components/move-selector/move-selector.component';
 
 @NgModule({
     declarations: [
