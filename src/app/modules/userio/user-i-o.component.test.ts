221559868dd51f2230fb469be844736061c51005
Martijn van Duijneveldt <martijnvanduijneveldt@gmail.com>
2019-05-24 12:58:47 +0200

    Updated user-i-o.component tests

--- src/app/modules/userio/user-i-o.component.test.ts
+++ src/app/modules/userio/user-i-o.component.test.ts
@@ -1,10 +1,79 @@
 import {UserIOComponent} from './user-i-o.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Pokemon} from '../../shared/models/pokemon/pokemon';
+import {PokemonType} from '../../shared/models/pokemon/pokemon-types';
+import {UserIOModule} from './user-i-o.module';
+import {AttackLog} from '../../shared/models/battle/attack-log';
+import {BehaviorSubject} from 'rxjs';
+import {AppRoutingModule} from '../../app-routing.module';
+import {PokeApiService} from '../../shared/services/pokeapi.service';
+import {PokedexService} from '../../shared/services/pokedex.service';
+import {StorageService} from '../../shared/services/storage.service';
+import {AppBaseHrefProvider} from '../../../test-helpers/app-base-href.provider';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
-describe('user-i-o-component Tests', () => {
-    let userIO: UserIOComponent;
+describe('UserIOComponent', () => {
+    let component: UserIOComponent;
+    let fixture: ComponentFixture<UserIOComponent>;
+    let logObservable: BehaviorSubject<AttackLog>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                AppRoutingModule,
+                BrowserAnimationsModule,
+                UserIOModule
+            ],
+            providers: [
+                PokeApiService,
+                PokedexService,
+                StorageService,
+                AppBaseHrefProvider,
+            ]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(UserIOComponent);
+        component = fixture.componentInstance;
+
+        component.pokemon = new Pokemon('Pikachu', 90, 50, 10, [PokemonType.electric]);
+
+        logObservable = new BehaviorSubject<AttackLog>(undefined);
+        component.logEvent = logObservable.asObservable();
+    }));
+
     it('should log message', () => {
-        userIO = new UserIOComponent();
-        userIO.onLogged('testlog');
-        expect(userIO.logs[0].message).toBe('testlog');
+        component.onLogged('testlog');
+        expect(component.logs[0].message).toBe('testlog');
     });
+
+    it('Should add message on event', () => {
+        component.ngOnInit();
+
+        logObservable.next(AttackLog.message('new message'));
+        expect(component.logs).toHaveLength(1);
+        component.ngOnDestroy();
+    });
+
+    it('Test selector width', () => {
+        const logDisplayer = fixture.nativeElement.querySelector('app-log-displayer');
+        component.moveSelectRequire = false;
+        component.exitSelectRequire = false;
+
+        fixture.detectChanges();
+        expect(logDisplayer.style.width).toBe('600px');
+
+        component.exitSelectRequire = true;
+        fixture.detectChanges();
+        expect(logDisplayer.style.width).toBe('400px');
+        component.exitSelectRequire = false;
+
+        component.moveSelectRequire = true;
+        fixture.detectChanges();
+        expect(logDisplayer.style.width).toBe('400px');
+        component.moveSelectRequire = false;
+    });
+
+
 });
--- src/app/modules/userio/user-i-o.component.ts
+++ src/app/modules/userio/user-i-o.component.ts
@@ -1,45 +1,51 @@
-import {Component, Input, OnInit, Output, ViewChild} from '@angular/core';
-import { Pokemon } from '../../shared/models/pokemon/pokemon';
+import {Component, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {Pokemon} from '../../shared/models/pokemon/pokemon';
 import {AttackLog} from '../../shared/models/battle/attack-log';
 import {LogDisplayerComponent} from '../../components/log-displayer/log-displayer.component';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {BehaviorSubject, Observable, Subscription} from 'rxjs';
 import {Move} from '../../shared/models/move/move';
 
 @Component({
-  selector: 'app-user-io',
-  templateUrl: './user-i-o.component.html',
-  styleUrls: ['./user-i-o.component.css']
+    selector: 'app-user-io',
+    templateUrl: './user-i-o.component.html',
+    styleUrls: ['./user-i-o.component.css']
 })
-export class UserIOComponent implements OnInit {
+export class UserIOComponent implements OnInit, OnDestroy {
 
-  @ViewChild(LogDisplayerComponent) logDisplayer: LogDisplayerComponent;
+    @ViewChild(LogDisplayerComponent) logDisplayer: LogDisplayerComponent;
 
-  @Input()
-  pokemon: Pokemon;
+    @Input()
+    pokemon: Pokemon;
 
-  @Input()
-  moveSelectRequire: boolean;
+    @Input()
+    moveSelectRequire: boolean;
 
-  @Input()
-  exitSelectRequire: boolean;
+    @Input()
+    exitSelectRequire: boolean;
 
-  @Input()
-  selectedMoveEvent: BehaviorSubject<Move>;
+    @Input()
+    selectedMoveEvent: BehaviorSubject<Move>;
 
-  logs: AttackLog[] = [];
+    logs: AttackLog[] = [];
 
-  @Input()
-  logEvent: Observable<AttackLog>;
+    @Input()
+    logEvent: Observable<AttackLog>;
 
-  ngOnInit(): void {
-    this.logEvent.subscribe(logCombat => {
-      if (logCombat) {
-        this.logs.push(logCombat);
-      }
-    });
-  }
+    private subscription: Subscription;
 
-  onLogged(log: string) {
-    this.logs.push(AttackLog.message(log));
-  }
+    ngOnInit(): void {
+        this.subscription = this.logEvent.subscribe(logCombat => {
+            if (logCombat) {
+                this.logs.push(logCombat);
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+    onLogged(log: string): void {
+        this.logs.push(AttackLog.message(log));
+    }
 }
